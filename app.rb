4051afc3d14a83c5907f9632a9667febc5913968
iaq16 <iaq16@hotmail.com>
2018-04-07 14:46:51 -0500

    Se crearon las cajas de texto.. y se actualiza la vista

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require './lib/Palabras.rb'
 
 get '/' do
 	 palabras=Palabras.new
-	 session["longitudpalabra"]= palabras.obtenerLongitud     
+	 session["longitudpalabra"]= palabras.obtenerLongitud
 	 erb :ahorcado
 end
